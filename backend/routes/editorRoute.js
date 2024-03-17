cbdfb899383042ca6e8c944f791bb8ff0d02287f
Yash Prajapati <workwidyash@gmail.com>
2024-03-17 12:44:36 +0530

    Added workspace & folder sharing [Backend]

--- backend/routes/editorRoute.js
+++ backend/routes/editorRoute.js
@@ -11,7 +11,11 @@ const {
 	getFilesByFolderID,
 	deleteFile,
 	getFileByID,
-	saveCode
+	saveCode,
+	addCollaboratorToWorkspace,
+	removeCollaboratorFromWorkspace,
+	addCollaboratorToFolder,
+	removeCollaboratorFromFolder
 } = require("../controller/editorCtrl");
 
 //initializing router
@@ -23,11 +27,15 @@ router.get("/editor/:workspaceID/:folderID/:fileID", getEditor) // TESTING
 router.post("/workspace/add",addWorkspace)
 router.get("/workspace/get/:userID", getWorkspacesByUserID)
 router.delete("/workspace/delete/:id",deleteWorkspace)
+router.post("/workspace/share/:workspaceID",addCollaboratorToWorkspace)
+router.delete("/workspace/share/:workspaceID",removeCollaboratorFromWorkspace)
 
 // APIs for Folder
 router.post("/folder/add/:workspaceID", addFolder)
 router.get("/folder/get-all/:workspaceID", getFoldersByWorkspaceID)
 router.delete("/folder/delete/:folderID", deleteFolder)
+router.post("/folder/share/:folderID",addCollaboratorToFolder)
+router.delete("/folder/share/:folderID",removeCollaboratorFromFolder)
 
 // APIs for File
 router.post("/file/add/:folderID", addFile)
